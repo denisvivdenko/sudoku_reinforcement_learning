2f73e7e406e60448c2ef11bbddbfa8eb7167ab34
denisvivdenko <denis.vivdenko.freelancer@gmail.com>
2022-03-25 13:05:35 +0100

    feat: add tests for is_solved

--- tests/test_sudoku.py
+++ tests/test_sudoku.py
@@ -13,3 +13,14 @@ def test_sudoku_is_solved_false():
     expected = False
     actual = sudoku.is_solved()
     assert expected == actual
+
+def test_sudoku_is_solved_true():
+    matrix = np.array([
+        [2, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8]
+    ])
+    sudoku = Sudoku(matrix)
+    expected = True
+    actual = sudoku.is_solved()
+    assert expected == actual
