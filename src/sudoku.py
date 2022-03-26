8e2052dd38242fdfcd183fa15b140c065ea94ed8
denisvivdenko <denis.vivdenko.freelancer@gmail.com>
2022-03-26 11:01:59 +0100

    feat: add line inspector method

--- src/sudoku.py
+++ src/sudoku.py
@@ -2,29 +2,30 @@ from collections import namedtuple
 from distutils.log import Log
 from enum import Enum
 import os
+from tkinter import HORIZONTAL
 
 import numpy as np
 
 from src.logger import Logger
 
 
-class RuleViolation(Exception):
+class RuleViolationError(Exception):
     """Base class for other rule violation exceptions."""
     pass
 
-class HorizontalLineContainsValue(RuleViolation):
+class HorizontalLineContainsValueError(RuleViolationError):
     """Raised when input value that already exists in horizontal line."""
     pass
 
-class VerticalLineContainsValue(RuleViolation):
+class VerticalLineContainsValueError(RuleViolationError):
     """Raised when input value that already exists in vertical line."""
     pass
 
-class SquareContainsValue(RuleViolation):
+class SquareContainsValueError(RuleViolationError):
     """Raised when input value that already exists in square."""
     pass
 
-class CellHasValue(RuleViolation):
+class CellHasValueError(RuleViolationError):
     """Raised when insert value into cell that already contains value."""
     pass
 
@@ -41,40 +42,50 @@ class Sudoku:
             sudoku_matrix (np.array): matrix (n x n) cells with (m x m) squares.
                 cell can contain numbers [1;9] and 0 if it is empty.  
         """
-        self.sudoku_matrix: np.array = sudoku_matrix
+        self._sudoku_matrix: np.array = sudoku_matrix
 
     def insert_value(self, value: int, cell: Cell) -> None:
         """
         Inserts given value to cell. 
-        If it is impossible raises one of RuleViolation exceptions.
+        If it is impossible raises one of RuleViolationError exceptions.
         
         Parameters:
             cell (Cell): namedtuple Cell(row=1, column=0).
             value (int): value to input.
         """
-
-        if self._line_contains_value(value, cell, Axis.HORIZONTAL):
-            raise HorizontalLineContainsValue()
+        if self._sudoku_matrix[cell.row, cell.column] != 0:
+            raise CellHasValueError()
+        elif self._line_contains_value(value, cell, Axis.HORIZONTAL):
+            raise HorizontalLineContainsValueError()
         elif self._line_contains_value(value, cell, Axis.VERTICAL):
-            raise VerticalLineContainsValue()
+            raise VerticalLineContainsValueError()
         elif self._square_contains_value(value, self._convert_cell_to_square_cell(cell)):
-            raise SquareContainsValue()
-        elif self.sudoku_matrix[cell.row, cell.column] != 0:
-            raise CellHasValue()
+            raise SquareContainsValueError()
         self.sudoku_matrix[cell.row, cell.column] = value
     
     def is_solved(self) -> bool:
         """If sudoku pazzle is solved returns True."""
-        if np.isin(0, self.sudoku_matrix):
+        if np.isin(0, self._sudoku_matrix):
             return False
         return True
 
     def _line_contains_value(self, value: int, cell: Cell, axis: Axis) -> bool:
         """
-        If line contains given value then returns True
+        If line contains given value then returns True.
+        
+        Parameters: 
+            cell (Cell): row and column starting from 0 to (n - 1).
+            value (int): searching value.
+            axis (Axis): HORIZONTAL or VERTICAL
         """
-        pass
-    
+        if axis == Axis.HORIZONTAL:
+            if np.isin(value, self._sudoku_matrix[cell.row]):
+                return True
+        elif axis == Axis.VERTICAL:
+            if np.isin(value, self._sudoku_matrix[:, cell.column]):
+                return True
+        return False
+
     def _convert_cell_to_square_cell(self, cell) -> int:
         """
             +-----------+
--- tests/test_sudoku.py
+++ tests/test_sudoku.py
@@ -2,8 +2,10 @@ import pytest
 import numpy as np
 
 from src.sudoku import Sudoku
+from src.sudoku import Cell
+from src.sudoku import HorizontalLineContainsValueError, VerticalLineContainsValueError
 
-def test_sudoku_is_solved_false():
+def test_sudoku_is_solved_false() -> None:
     matrix = np.array([
         [0, 1, 2],
         [3, 4, 5],
@@ -14,7 +16,7 @@ def test_sudoku_is_solved_false():
     actual = sudoku.is_solved()
     assert expected == actual
 
-def test_sudoku_is_solved_true():
+def test_sudoku_is_solved_true() -> None:
     matrix = np.array([
         [2, 1, 2],
         [3, 4, 5],
@@ -24,3 +26,25 @@ def test_sudoku_is_solved_true():
     expected = True
     actual = sudoku.is_solved()
     assert expected == actual
+
+def test_sudoku_exception_inserting_value_horizontal_line() -> None:
+    matrix = np.array([
+        [0, 1, 2],
+        [0, 4, 5],
+        [6, 7, 8]
+    ])
+    sudoku = Sudoku(matrix)
+    cell = Cell(row=0, column=0)
+    with pytest.raises(HorizontalLineContainsValueError):
+        sudoku.insert_value(value=2, cell=cell)
+
+def test_sudoku_exception_inserting_value_vertical_line() -> None:
+    matrix = np.array([
+        [0, 1, 2],
+        [0, 4, 5],
+        [6, 7, 8]
+    ])
+    sudoku = Sudoku(matrix)
+    cell = Cell(row=0, column=0)
+    with pytest.raises(VerticalLineContainsValueError):
+        sudoku.insert_value(value=6, cell=cell)
